1c7ae1850d7c86490473049d1e7c0dc64a0e3e85
arpatterson0 <36012005+arpatterson0@users.noreply.github.com>
2018-05-20 09:01:48 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-import express from 'express';
-import { mongoose } from 'mongoose';
-import bodyPaser from 'body-parser';
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import routes from "./src/routes/routes";
+
 const app = express();
-const port = 3000;
-mongoose.connect('mongodb://localhostproject2');
+const PORT = 3000;
+
+//mongo connection
+mongoose.connect("mongodb://localhost/project2");
+
+//bodyparser
+app.use(bodyParser.urlencoded({ extended: true })); //parse incoming requests with urlencoded payloads
+app.use(bodyParser.json()); //parse incoming requests with json payloads
 
 routes(app);
-app.get('/', (req, res) => {
-    res.send('Node and express server running on port: ${PORT}')
+
+//prints stuff to homepage
+app.get("/", (req, res) => {
+  res.send(`Node and express server running on port: ${PORT}`);
 });
 
+//server is listening on this port
+app.listen(PORT, () => {
+  console.log(`the server is up and running on localhost:${PORT}`);
+});
