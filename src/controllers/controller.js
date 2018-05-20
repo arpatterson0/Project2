34a0e88af3f56a5e7c3ad8cdf516f2a01415b860
arpatterson0 <36012005+arpatterson0@users.noreply.github.com>
2018-05-20 08:56:33 -0400

    Add files via upload

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -12,3 +12,47 @@ export const addNewContact = (req,res) => {
         res.json(contact);
     });
 };
+//gets all contacts from database
+export const getContacts = (req, res) => {
+  Contact.find({}, (err, contact) => {
+    if (err) {
+      res.send(err);
+    }
+    res.json(contact);
+  });
+};
+
+//gets a contact by _id
+export const getSpecificContact = (req, res) => {
+  Contact.findById(req.params.contactId, (err, contact) => {
+    if (err) {
+      res.send(err);
+    }
+    res.json(contact);
+  });
+};
+
+//update a contact in the database
+export const updateContact = (req, res) => {
+  Contact.findOneAndUpdate(
+    { _id: req.params.contactId },
+    req.body,
+    { new: true },
+    (err, contact) => {
+      if (err) {
+        res.send(err);
+      }
+      res.json(contact);
+    }
+  );
+};
+
+//delete a contact from the database
+export const deleteContact = (req, res) => {
+  Contact.remove({ _id: req.params.contactId }, (err, contact) => {
+    if (err) {
+      res.send(err);
+    }
+    res.json({ message: "You have deleted a contact" });
+  });
+};
