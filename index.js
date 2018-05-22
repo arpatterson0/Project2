6ac1906f4e82a567462de6a5aa483e976839c286
arpatterson0 <36012005+arpatterson0@users.noreply.github.com>
2018-05-22 07:59:48 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ const PORT = 3000;
 
 //mongo connection
 mongoose.connect("mongodb://localhost/project2");
-
+app.use(function(req,res, next){
+  res.header('Access-Control-Allow-Origin',"*");
+  res.header('Access-Control-Allow-Method','GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type);')
+});
 //bodyparser
 app.use(bodyParser.urlencoded({ extended: true })); //parse incoming requests with urlencoded payloads
 app.use(bodyParser.json()); //parse incoming requests with json payloads
@@ -24,4 +28,7 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log(`the server is up and running on localhost:${PORT}`);
 });
+
+
+
 
