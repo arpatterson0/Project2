3db7c78954a49bda78e163a234bbf55737cef54b
arpatterson0 <36012005+arpatterson0@users.noreply.github.com>
2018-05-22 08:00:07 -0400

    Add files via upload

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,24 +1,31 @@
 import {
-  addNewContact,
-  getContacts,
-  getSpecificContact,
-  updateContact,
-  deleteContact
-} from "../controllers/controller";
-
-const routes = app => {
-  //Contact route (whole collection or posting to db)
-  app
-    .route("/contact")
-    .get(getContacts)
-    .post(addNewContact);
-
-  //specific id's in database
-  app
-    .route("/contact/:contactId")
-    .get(getSpecificContact)
-    .put(updateContact)
-    .delete(deleteContact);
-};
-
-export default routes;
+    addNewContact,
+    getContacts,
+    getSpecificContact,
+    updateContact,
+    deleteContact
+  } from "../controllers/controller";
+  
+  const routes = app => {
+    //Contact route (whole collection or posting to db)
+    app
+      .route("/contacts")
+      .get(getContacts)
+      .post(addNewContact);
+      var contacts=[{
+        "Firstname": "bob",
+      "lastname": "jones",
+      "email": "bob@gmail.com",
+      "Phonenumber": "240-555-5555",
+      "company": "jersey",
+      "date":"10/12/2017",
+      }]
+    //specific id's in database
+    app
+      .route("/contacts/:contactId")
+      .get(getSpecificContact)
+      .put(updateContact)
+      .delete(deleteContact);
+  };
+  
+  export default routes;
